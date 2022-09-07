571f518b988a68de13caedc24945d68c53485f37
황지훈 <rkgh17@gmail.com>
2022-09-07 10:09:11 +0900

    day22_1t

--- myOracle.sql
+++ myOracle.sql
@@ -1,32 +1,10 @@
 /*
-CRUD
+INSERT
+insert into 테이블명 values(값1, 값2, 값3....);
 
-drop table : schema+data가 날아감
-truncate table : data만 날아감
+원하는 컬럼에 데이터 넣기
+insert into 테이블명(컬럼명1, ...컬럼명n) values(값1, .... 값n);
+
+MySQL의 경우 한 가지 방법 더 지원
+insert into 테이블명 set 컬럼명1=값1, ...... 컬럼명n=값n;
 */
-select table_name from user_tables; -- 유저가 만든 테이블 리스트
-
-/*
-UPDATE(ORACLE)
-1. 테이블명 수정 : rename 테이블명 to 바꿀테이블명
-
-2. (스키마)컬럼 수정
-    컬럼이름 추가 : ALTER TABLE 테이블명 ADD /추가할 컬럼 타입/
-    컬럼 삭제 : ALTER TABLE 테이블명 DROP COLUMN /삭제할 컬럼명/
-    컬럼이름 변경 : ALTER TABLE 테이블명 RENAME COLUMN 컬럼명 TO 새컬럼명 - 타입은 그대로
-    컬럼타입 변경 : ALTER TABLE 테이블명 MODIFY 기존컬럼명 /바꿀데이터타입/
-    
-
-UPDATE(MySQL)
-MySQL은 컬럼 이름/타입/길이 수정이 ORACLE보다 간단
-    컬럼 변경 : ALTER TABLE 테이블명 CHANGE 컬럼명 /새컬럼명 새타입 새길이/
-    ex) alter table std change mobile phone varchar(12) -> mobile컬럼을 varchar(12)인 phone으로 바꿈
-        alter table std change mobile phone phone char(24) -> 컬럼 수정
-*/
-
-CREATE TABLE STUDENT(
-NAME VARCHAR2(12),
-GENDER CHAR(1)
-);
-RENAME STUDENT TO STD;
-select * from std;
