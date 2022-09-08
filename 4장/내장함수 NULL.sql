719decc0b2d0e94e94f585aec3d347291ada7d8e
황지훈 <rkgh17@gmail.com>
2022-09-08 12:45:37 +0900

    day23_4T

---
+++ 4장/내장함수 NULL.sql
@@ -0,0 +1,28 @@
+-- BUILTIN FUNTION NULL
+
+/* ★NVL(컬럼명, n) : 컬럼값이 NULL아니면, 그 칼럼값 출력 
+                                   컬럼값이 NULL이면, n 출력. 이때 n은 컬럼의 데이터형식을 따라야 함
+*/
+
+SELECT EMP_NAME, MANAGER_ID FROM EMPLOYEES;
+SELECT EMP_NAME, NVL(MANAGER_ID, '0') FROM EMPLOYEES;
+
+/* NVL2(컬럼명, n1, n2) : 칼럼값이 NULL이면 n1
+                                          NULL이면 n2
+*/
+SELECT EMP_NAME,MANAGER_ID, NVL2(MANAGER_ID, manager_id, '0') FROM EMPLOYEES;
+SELECT EMP_NAME,MANAGER_ID, NVL2(MANAGER_ID, 1, '0') FROM EMPLOYEES;
+SELECT EMP_NAME, COMMISSION_PCT FROM EMPLOYEES;
+
+-- COALESCE : NULL이 아닌 첫 번째 표현식을 반환
+SELECT EMP_NAME,SALARY, COMMISSION_PCT, COALESCE((COMMISSION_PCT+1)*SALARY,SALARY) AS SALARY2 FROM EMPLOYEES;
+
+/* LNNVL(조건식) : 조건식이 FALSE나 UNKNOWN이면 TRUE
+                                  TRUE면 FALSE 반환 */
+SELECT TO_CHAR(COMMISSION_PCT,'99.99') FROM EMPLOYEES WHERE NVL(COMMISSION_PCT,0) <0.2;
+SELECT TO_CHAR(COMMISSION_PCT,'99.99') FROM EMPLOYEES WHERE LNNVL(COMMISSION_PCT>=0.2); -- 위의 결과와 같음
+
+/*
+NULLIF(n1,n2) : n1 == n2 면 NULL
+                   n1 <>  n2면 n1출력
+*/
