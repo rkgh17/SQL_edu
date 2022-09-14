a99c572202903fb14ba8509a6b02f04e892710b0
황지훈 <rkgh17@gmail.com>
2022-09-14 15:52:41 +0900

    day25_6t

--- myOracle.sql
+++ myOracle.sql
@@ -18,27 +18,64 @@ END LOOP;
 
 */
 
+--WHILE
+
 DECLARE
-    VN_SALARY EMPLOYEES.SALARY%TYPE;
-    VN_DID departments.department_id%TYPE;
+    VN_X INTEGER :=1;
+    TOTAL INTEGER:=0;
 BEGIN
-    SELECT SALARY INTO VN_SALARY
-    FROM EMPLOYEES
-    WHERE DEPARTMENT_ID = 90 AND ROWNUM=1;
-    
-    DBMS_OUTPUT.PUT_LINE(VN_SALARY);
-    
-    CASE
-        WHEN VN_SALARY BETWEEN 1 AND 3000 THEN
-            DBMS_OUTPUT.PUT_LINE('낮음');
-        WHEN VN_SALARY BETWEEN 3001 AND 6000 THEN
-            DBMS_OUTPUT.PUT_LINE('중간');
-        WHEN VN_SALARY BETWEEN 6001 AND 10000 THEN
-            DBMS_OUTPUT.PUT_LINE('높음');
-        ELSE
-            DBMS_OUTPUT.PUT_LINE('최상위');
-    END CASE;
+    WHILE VN_X <= 100
+    LOOP
+        TOTAL:=TOTAL+VN_X;
+        VN_X:=VN_X+1;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('TOTAL = '||TOTAL);
 END;
 /
 
+/*구구단 2단 - WHILE*/
+DECLARE
+    VN_X INTEGER := 2;
+    VN_Y INTEGER := 1;
+BEGIN
+    WHILE VN_Y<10
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(VN_X|| ' x ' ||VN_Y||' = '||(VN_X*VN_Y));
+        VN_Y:=VN_Y+1;
+    END LOOP;
+END;
+/
 
+/*구구단 2단 - LOOP*/
+DECLARE
+    VN_X INTEGER := 2;
+    VN_Y INTEGER := 1;
+BEGIN
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(VN_X|| ' x ' ||VN_Y||' = '||(VN_X*VN_Y));
+        EXIT WHEN VN_Y=9;
+        VN_Y:=VN_Y+1;
+    END LOOP;
+END;
+/
+
+/*구구단*/
+DECLARE
+    VN_X INTEGER := 2;
+    VN_Y INTEGER := 1;
+    V_TMP VARCHAR2(1000);
+BEGIN
+    V_TMP:=CHR(10);
+    WHILE VN_X<10
+    LOOP
+        WHILE VN_Y<10
+        LOOP
+        DBMS_OUTPUT.PUT_LINE(VN_X|| ' x ' ||VN_Y||' = '||(VN_X*VN_Y));
+        VN_Y:=VN_Y+1;
+        END LOOP;
+        VN_Y:=1;
+        VN_X:=VN_X+1;
+        DBMS_OUTPUT.PUT_LINE(V_TMP);
+    END LOOP;
+END;
+/
